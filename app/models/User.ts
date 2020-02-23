72bf7972206d0ecbef211628b813c644616c71ca
Magmiv <ivan.mag.web@gmail.com>
2020-02-23 16:47:22 +0300

    Removing extra files

--- app/models/User.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-// TODO image
-const UserSchema = new mongoose.Schema({
-   name: String,
-   password: String,
-});
-
-export default mongoose.model("User", UserSchema, "users");
